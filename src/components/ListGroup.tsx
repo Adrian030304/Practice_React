0e3091378f71c6505165be1dd906db20b40a1784
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-02 00:50:09 +0200

    handle function on element click and conditional css class

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,28 @@
 
+import { MouseEvent } from "react";
 
 
 function ListGroup() {
     let items: string[] = ["Titu", "Brasov", "Bucuresti"]
-    items = []
+    let selectedIndex = 0;
+    const handleClick = (event:MouseEvent) => {
+        console.log(event)
+    }
 
     return (
     <>
     <h1>List</h1>
     {items.length === 0 && <p>No items found</p>}
     <ul className="list-group">
-        {items.map(item => (
-        <li key={item}>{item}</li>
+        {items.map((item, index) => (
+        <li 
+        className={ 
+            index % 2 === selectedIndex 
+            ? 'list-group-item active'
+            : 'list-group-item'} 
+            onClick={handleClick} 
+            key={item}>{item}
+        </li>
         ))}
     </ul>
     </>
