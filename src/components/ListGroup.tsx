6445dea3b4fc84b055c6b8a39131d71ed3e185ae
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-02 00:36:51 +0200

    conditional rendering jeyword

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,14 +5,10 @@ function ListGroup() {
     let items: string[] = ["Titu", "Brasov", "Bucuresti"]
     items = []
 
-    const getMessage = () => {
-        return items.length === 0 ? <p>No item found</p>: null;
-    }
-
     return (
     <>
     <h1>List</h1>
-    {getMessage()}
+    {items.length === 0 && <p>No items found</p>}
     <ul className="list-group">
         {items.map(item => (
         <li key={item}>{item}</li>
