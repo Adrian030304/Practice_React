01c3c73b2ce0d21050e085c66d01d97d31363d80
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-02 00:57:00 +0200

    using react hooks

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 
 function ListGroup() {
     let items: string[] = ["Titu", "Brasov", "Bucuresti"]
-    let selectedIndex = 0;
-    const handleClick = (event:MouseEvent) => {
-        console.log(event)
-    }
-
+    
+    // Hook - we can tell react that this component has data that can change over time
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    // arr[0] - variable (selectedIndex)
+    // arr[1] - updater function
+    const [name, setName] = useState('')
     return (
     <>
     <h1>List</h1>
@@ -17,10 +18,11 @@ function ListGroup() {
         {items.map((item, index) => (
         <li 
         className={ 
-            index % 2 === selectedIndex 
-            ? 'list-group-item active'
-            : 'list-group-item'} 
-            onClick={handleClick} 
+                index % 2 === selectedIndex 
+                ? 'list-group-item active'
+                : 'list-group-item'} 
+            onClick={() => {setSelectedIndex(selectedIndex + 1)
+            }} 
             key={item}>{item}
         </li>
         ))}
